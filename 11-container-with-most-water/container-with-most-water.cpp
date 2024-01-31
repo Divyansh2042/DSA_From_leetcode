36e1f3a6c59dbe904476aad0a5254bd7b93ab6fa
Divyansh kumar singh <93935509+Divyansh2042@users.noreply.github.com>
2024-01-31 19:20:16 +0530

    Time: 58 ms (92.78%) | Memory: 61.4 MB (5.67%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+       //Brute Force Approach
+    //    int max1=0,area; 
+    //    for(int i=0;i<height.size();i++){
+    //        for(int j=i+1;j<height.size();j++){
+    //             area=(j-i)*min(height[i],height[j]);
+    //             max1=max(max1,area);
+    //        }
+    //    }
+    //    return max1;
+    //Optimal Appraoch
+    int max1=0,area;;
+    int left=0,right=height.size()-1;
+    while(left<right){
+        area=(right-left)*min(height[left],height[right]);
+        max1=max(max1,area);
+        if(height[left]<height[right]){
+            left++;
+        }else {
+            right--;
+        }
+    }
+    return max1;
+    }
+};

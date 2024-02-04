94a8488185b76875c374d58153ccc30cce8464eb
Divyansh kumar singh <93935509+Divyansh2042@users.noreply.github.com>
2024-02-04 22:52:10 +0530

    Time: 26 ms (24.61%) | Memory: 10.5 MB (9.91%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+     if(s.empty()||t.empty()){
+         return "";
+     }
+     unordered_map<char,int>dictT;
+     for(char c:t){
+         int count=dictT[c];
+         dictT[c]=count+1;
+     }
+     int required=dictT.size();
+     int l=0,r=0;
+     int formed=0;
+     unordered_map<char,int>windowCount;
+     int ans[3]={-1,0,0};
+     while(r<s.length()){
+         char c=s[r];
+         int count=windowCount[c];
+         windowCount[c]=count+1;
+         if(dictT.find(c)!=dictT.end() && windowCount[c]==dictT[c]){
+             formed++;
+         }
+         while(l<=r && formed == required){
+             c=s[l];
+             if(ans[0]==-1 || r-l+1<ans[0]){
+                 ans[0]=r-l+1;
+                 ans[1]=l;
+                 ans[2]=r;
+             }
+             windowCount[c]--;
+              if(dictT.find(c)!=dictT.end() && windowCount[c]<dictT[c]){
+             formed--;
+         }
+         l++;
+         }
+         r++;
+     }
+     return ans[0]==-1?"":s.substr(ans[1],ans[0]);
+    }
+};

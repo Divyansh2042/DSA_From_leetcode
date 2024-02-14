7a32e6688954a51a7b48186dfa4980ffa2b83221
Divyansh kumar singh <93935509+Divyansh2042@users.noreply.github.com>
2024-02-14 09:42:48 +0530

    Time: 135 ms (91.67%) | Memory: 137 MB (18.51%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        //Brute Force Approach
         int n=nums.size();
-        vector<int> ans(n);
-        for(int i=0,j=0,k=0; i<n; i++){
-            if(i%2==0){
-                while(nums[j]<0){ j++; }
-                ans[i]=nums[j++];
+        vector<int>pos;
+        vector<int>neg;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                pos.push_back(nums[i]);
             }else{
-                while(nums[k]>0){ k++; }
-                ans[i]=nums[k++];
+                neg.push_back(nums[i]);
             }
         }
-        return ans;
+        int j=0;
+        for(int i=0;i<n;i+=2){
+            nums[i]=pos[j];
+            nums[i+1]=neg[j];
+            j++;
+        }
+        return nums;
     }
 };

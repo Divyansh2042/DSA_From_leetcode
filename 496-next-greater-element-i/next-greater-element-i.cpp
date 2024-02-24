1513c16c0514f99376496bd948a55c4bea006a42
Divyansh kumar singh <93935509+Divyansh2042@users.noreply.github.com>
2024-02-24 22:17:09 +0530

    Time: 3 ms (86.61%) | Memory: 12.1 MB (18.68%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-      vector<int>ans;
-      int m=nums1.size();
-      int n=nums2.size();
-      for(int i=0;i<m;i++){
-          int x=find(nums2.begin(),nums2.end(),nums1[i])-nums2.begin();
-          int flag=0;
-          for(int j=x;j<n;j++){
-              if(nums2[j]>nums1[i]&&flag==0){
-                  flag=1;
-                  ans.push_back(nums2[j]);
-              }
-          }
-          if(flag==0)ans.push_back(-1);  
-      }
-     return ans;
+        stack<int>s;
+        unordered_map<int,int>umap;
+        for(auto n:nums2){
+            while(s.size()&&s.top()<n){
+                umap[s.top()]=n;
+                s.pop();
+            }
+            s.push(n);
+
+        }
+        vector<int>ans;
+        for(int n:nums1)ans.push_back(umap.count(n)?umap[n]:-1);
+        return ans;
     }
 };

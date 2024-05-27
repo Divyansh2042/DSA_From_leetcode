abf65fe412668bf7a2ae35fc56eddb0837d9fefd
Divyansh kumar singh <93935509+Divyansh2042@users.noreply.github.com>
2024-05-27 08:52:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (53.24%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-        int n=nums.size();
         // sort(nums.begin(),nums.end());
-        int l=1,r=101,res=-1;
-        function<int(int)>safe=[&](int mid){
-            int cnt=0;
-            for(int  i=0;i<n;i++)cnt+=(nums[i]>=mid);
-            return cnt;
-        };
-        while(l<r){
-            int mid=(l+r)/2;
-            int cnt=safe(mid);
-            if(cnt==mid)return mid;
-                // res=mid;
-                cout<<mid<<" "<<cnt<<endl;
-            if(cnt>mid){
-                l=mid+1;
+        int n=nums.size(),ans=n;
+        while(ans){
+            int chk=0;
+            for(int i=0;i<n;i++){
+               if(nums[i]>=ans){chk++;} 
             }
-            else{
-                r=mid;
-            }
+            if(chk==ans){return ans;}
+            ans--;
         }
-
-        return res;
+        return -1;
     }
 };
